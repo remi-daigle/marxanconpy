553a421b70e6f49a18b9ea297dcf6fcabda25739
Remi Daigle <remi.daigle@dal.ca>
2018-10-16 11:11:16 -0400

    update makefile

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 import setuptools
 
-setuptools.setup(name='marxanconpy',
-                 version='0.1.2b1',
-                 description='The python package for Marxan Connect',
-                 url='marxanconnect.ca',
-                 author='Remi Daigle',
-                 author_email='remi.daigle@dal.ca',
-                 license='MIT',
-                 packages=setuptools.find_packages(),
-                 classifiers=[
-                     "Programming Language :: Python :: 3",
-                     "License :: OSI Approved :: MIT License",
-                     "Operating System :: OS Independent",
-                 ],
-                 )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='marxanconpy',
+    version='0.1.2b1',
+    author='Remi Daigle',
+    author_email='remi.daigle@dal.ca',
+    description="The python package for Marxan Connect",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://remi-daigle.github.io/marxanconpy/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
